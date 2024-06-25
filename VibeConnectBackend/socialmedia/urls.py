22551e88de191666d2348e8106fcb4ee6151b417
niyati <niyupathak1503@gmail.com>
2024-06-25 13:44:10 +0530

    api for registeration.

--- VibeConnectBackend/socialmedia/urls.py
+++ VibeConnectBackend/socialmedia/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from socialmedia.views import *
+from socialmedia.api_view import *
+from . import api_view
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 urlpatterns = [
 path('login/',TokenObtainPairView.as_view(),name='token_obtain'),
-path('refresh/',TokenRefreshView.as_view(),name='token_refresh')
+path('refresh/',TokenRefreshView.as_view(),name='token_refresh'),
+path('signup/',api_view.register,name='signup')
+
 ]
