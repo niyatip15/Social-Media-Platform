33d9be4f47ecfb60b7096f595b2974762615987f
niyati <niyupathak1503@gmail.com>
2024-06-25 13:30:46 +0530

    set urls.

--- VibeConnectBackend/socialmedia/urls.py
+++ VibeConnectBackend/socialmedia/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from socialmedia.views import *
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 urlpatterns = [
-# path()
+path('login/',TokenObtainPairView.as_view(),name='token_obtain'),
+path('refresh/',TokenRefreshView.as_view(),name='token_refresh')
 ]
